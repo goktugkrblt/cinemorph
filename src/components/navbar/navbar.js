a5c77cc9512b32c8e9f1e9a57e9f77f44ae8a180
goktugkrblt <goktugkarabulut97@gmail.com>
2024-04-01 17:26:47 +0300

    added search-suggestions

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,27 +4,36 @@ import LogoSvg from '../assets/logo';
 import CloseSvg from '../assets/close';
 import MovieModal from '../movieModal';
 
-
-const searchMovies = async (query) => {
-  const apiKey = process.env.REACT_APP_TMDB_API_KEY;
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
-
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data.results;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return [];
-  }
-};
-
 const Navbar = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isSearchOpen, setIsSearchOpen] = useState(false); 
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [randomMovies, setRandomMovies] = useState([]);
+
+  useEffect(() => {
+    const getRandomMovies = async () => {
+      const apiKey = process.env.REACT_APP_TMDB_API_KEY;
+      const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
+
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const randomMovieTitles = data.results.slice(0, 6).map(movie => movie.title);
+        setRandomMovies(randomMovieTitles);
+      } catch (error) {
+        console.error('Error fetching random movies:', error);
+        setRandomMovies([]);
+      }
+    };
+
+    getRandomMovies();
+
+    return () => {
+      // Cleanup işlemi yapılabilir, şu an için gerek yok
+    };
+  }, []);
 
   useEffect(() => {
     if (isModalOpen || isSearchOpen) {
@@ -42,8 +51,17 @@ const Navbar = () => {
       return;
     }
 
-    const results = await searchMovies(query);
-    setSearchResults(results);
+    const apiKey = process.env.REACT_APP_TMDB_API_KEY;
+    const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
+
+    try {
+      const response = await fetch(searchUrl);
+      const data = await response.json();
+      setSearchResults(data.results);
+    } catch (error) {
+      console.error('Error searching movies:', error);
+      setSearchResults([]);
+    }
   };
 
   const openModal = (movie) => {
@@ -64,20 +82,6 @@ const Navbar = () => {
     setIsSearchOpen(!isSearchOpen);
   };
 
-  useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (event.key === 'Escape' && isModalOpen) {
-        closeModal();
-      }
-    };
-
-    window.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [isModalOpen]);
-
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className='navbar_left-content'>
@@ -88,11 +92,11 @@ const Navbar = () => {
       </div>
     
       <div className='navbar_right-content'>
-      <div className='navbar_search-content'>
-        <svg className='search-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="20px" height="20px" onClick={toggleSearch}>
-          <path style={{ fill: '#000' }} d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"/>
-        </svg>
-      </div>
+        <div className='navbar_search-content'>
+          <svg className='search-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="20px" height="20px" onClick={toggleSearch}>
+            <path style={{ fill: '#000' }} d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"/>
+          </svg>
+        </div>
         <div className='navbar_right-content_user_logo'>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="48px" height="48px">
             <circle cx="24" cy="24" r="20" fill="#000"/>
@@ -105,27 +109,33 @@ const Navbar = () => {
       </div>
       {isSearchOpen && ( 
         <div className="search-results">
-          <div className='search-content'>
-
-          <input
-            className='search-bar'
-            type="text"
-            placeholder="Search..."
-            value={searchQuery}
-            onChange={handleSearch}
-          />
-          {searchQuery.length > 0 && (
-            <div className="clear-button" onClick={() => setSearchQuery('')}><CloseSvg /></div>
-          )}
-               </div>
+         <div className='search-content'>
+            <input
+              className='search-bar'
+              type="text"
+              placeholder="Search..."
+              value={searchQuery}
+              onChange={handleSearch}
+            />
+            {searchQuery.length > 0 && ( 
+              <div className="clear-button" onClick={() => { setSearchQuery(''); setSearchResults([]); }}>
+                <CloseSvg />
+              </div>
+            )}
+          </div>
+          <div className='search-suggestions'>
+            <h1 className='search-suggestions-title'>Search Suggestions</h1>
+            {randomMovies.map((movie, index) => (
+              <div className='search-suggestions-link' key={index}>{movie}</div>
+            ))}
+          </div>
           <div className="movies">
-          {searchResults.map(movie => (
-            <div className="movie" key={movie.id} onClick={() => openModal(movie)}>
-              <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={movie.title} />
-              <p className='search-result-movie-title'>{movie.title}</p>
-            </div>
-          ))}
-            
+            {searchResults.map(movie => (
+              <div className="movie" key={movie.id} onClick={() => openModal(movie)}>
+                <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={movie.title} />
+                <p className='search-result-movie-title'>{movie.title}</p>
+              </div>
+            ))}
           </div>
         </div>
       )}
