fdd3f25d611477a172ac41d4ad2d46003058f018
goktugkrblt <goktugkarabulut97@gmail.com>
2024-04-21 01:22:21 +0300

    changed user-login file

--- src/components/user-login/UserConnect.js
+++ src/components/user-login/UserConnect.js
@@ -4,39 +4,29 @@ import './userconnect.css';
 const UserLogin = ({ onClose }) => {
   const [activeTab, setActiveTab] = useState('login');
 
-  React.useEffect(() => {
-    document.body.style.overflow = 'hidden';
-    return () => {
-      document.body.style.overflow = 'unset';
-    };
-  }, []);
-
   return (
-    <div className="user-modal">
-        <div className='user-modal-w'>
-           <div className="user-modal-content">
-      
-                  <div className="tabs">
-                        <button className={`loginBtn ${activeTab === 'login' ? 'active' : ''}`} onClick={() => setActiveTab('login')}>Login</button>
-                        <button className={`signupBtn ${activeTab === 'signup' ? 'active' : ''}`} onClick={() => setActiveTab('signup')}>Sign up</button>
-                  </div>
-                  {activeTab === 'login' && (
-                    <form>
-                      <input type="email" placeholder="E-mail" />
-                      <input type="password" placeholder="Password" />
-                      <button type="submit">Submit</button>
-                    </form>
-                  )}
-                  {activeTab === 'signup' && (
-                    <form>
-                      <input type="text" placeholder="Username" />
-                      <input type="email" placeholder="E-mail" />
-                      <input type="password" placeholder="Password" />
-                      <button type="submit">Sign up</button>
-                    </form>
-                  )}
-                  <button className="close-btn" onClick={onClose}>Close</button>
-          </div>
+    <div className="user-login-page">
+      <div className='user-login-content'>
+        <div className="tabs">
+          <button className={`loginBtn ${activeTab === 'login' ? 'active' : ''}`} onClick={() => setActiveTab('login')}>Login</button>
+          <button className={`signupBtn ${activeTab === 'signup' ? 'active' : ''}`} onClick={() => setActiveTab('signup')}>Sign up</button>
+        </div>
+        {activeTab === 'login' && (
+          <form>
+            <input type="email" placeholder="E-mail" />
+            <input type="password" placeholder="Password" />
+            <button type="submit">Submit</button>
+          </form>
+        )}
+        {activeTab === 'signup' && (
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="E-mail" />
+            <input type="password" placeholder="Password" />
+            <button type="submit">Sign up</button>
+          </form>
+        )}
+        <button className="close-btn" onClick={onClose}>Close</button>
       </div>
     </div>
   );
