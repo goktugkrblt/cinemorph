d0793639b4354a9c8dfd36c8d2050c13d4496aa4
goktugkrblt <goktugkarabulut97@gmail.com>
2024-04-21 01:21:13 +0300

    added router navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React, { useState } from 'react';
 import './App.css';
 import './main.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/navbar';
 import Slider from './components/slider/slider';
 import PopularFilm from './components/popular-film/popularFilm';
@@ -10,22 +12,50 @@ import FilmSection2 from './components/film-section2/filmSection2';
 import NowPlaying from './components/now-playing/nowPlaying';
 import AllFilms from './components/all-film/allFilm';
 import Footer from './components/footer/footer';
-
-
+import UserLogin from './components/user-login/UserConnect';
+import { useLocation } from 'react-router-dom';
 
 function App() {
+  const [userOpened, setUserOpened] = useState(false);
+
+  return (
+    <BrowserRouter>
+      <AppContent userOpened={userOpened} setUserOpened={setUserOpened} />
+    </BrowserRouter>
+  );
+}
+
+function AppContent({ userOpened, setUserOpened }) {
+  const location = useLocation();
+
+  // Kontrol edilen rotaları bir diziye koyun
+  const hideComponentsRoutes = ['/user'];
+
+  // Mevcut yolun, gizlenmesi gereken rotalardan biri olup olmadığını kontrol edin
+  const shouldHideComponents = hideComponentsRoutes.includes(location.pathname);
+
   return (
     <div className="App">
-        <Navbar />
-        <Slider />
-        <PopularFilm />
-        <FilmSection1 />
-        <TopRatedFilm />
-        <UpComingFilm />
-        <FilmSection2 />
-        <NowPlaying />
-        <AllFilms />
-        <Footer />  
+      <Navbar onLogoClick={() => {}} />
+      <Routes>
+        <Route
+          path="/user"
+          element={<UserLogin onClose={() => setUserOpened(false)} />}
+        />
+      </Routes>
+      {!userOpened && !shouldHideComponents && (
+        <>
+          <Slider />
+          <PopularFilm />
+          <FilmSection1 />
+          <TopRatedFilm />
+          <UpComingFilm />
+          <FilmSection2 />
+          <NowPlaying />
+          <AllFilms />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
